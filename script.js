da83adaee0d1b9dcf383b0dce662d589e717f6a1
black-fish8 <matilmayank@gmail.com>
2025-03-02 01:05:06 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+// JavaScript for HealthCare Pro website
+
+// Function to show an alert when the "Learn More" button is clicked
+function showAlert() {
+    alert('Welcome to HealthCare Pro! Your health is our top priority.');
+}
